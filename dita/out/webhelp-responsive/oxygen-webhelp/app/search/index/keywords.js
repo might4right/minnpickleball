e385b5708550126a711bc011e894b2e84d69f0a2
pet01448 <pet01448@umn.edu>
2019-11-16 00:00:35 -0600

    DITA files galore.

---
+++ dita/out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,16 @@
+define(function() {var keywords=[{w:"Mechanics",p:["p0"]},{w:"of",p:["p0","p4"]},{w:"the",p:["p0","p1","p4","p5","p7","p8"]},{w:"game",p:["p0","p4"]},{w:"doubles",p:["p1","p4"]},{w:"serving",p:["p1","p3","p4","p5"]},{w:"sequence",p:["p1","p5"]},{w:"Making",p:["p2"]},{w:"line",p:["p2"]},{w:"calls",p:["p2"]},{w:"basics",p:["p3"]},{w:"at",p:["p4"]},{w:"start",p:["p4"]},{w:"a",p:["p4"]},{w:"singles",p:["p5"]},{w:"Standard",p:["p6"]},{w:"scoring",p:["p6"]},{w:"kitchen",p:["p7"]},{w:"two-bounce",p:["p8"]},{w:"rule",p:["p8"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3];
+ph["p1"]=[2, 4, 5, 6];
+ph["p2"]=[7, 8, 9];
+ph["p3"]=[5, 10];
+ph["p4"]=[5, 11, 2, 12, 1, 13, 4, 3];
+ph["p5"]=[2, 14, 5, 6];
+ph["p6"]=[15, 16];
+ph["p7"]=[2, 17];
+ph["p8"]=[2, 18, 19];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
